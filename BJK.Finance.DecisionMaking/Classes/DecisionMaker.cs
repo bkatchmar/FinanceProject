99297f8d755c060ef99967cedf1e0e8f0d5a68e9
Brian Katchmar <bkatchmar@interactiveedge.com>
2025-04-01 08:36:29 -0400

    Make the decision class more flexible with flags to determine which strategies I want included

--- BJK.Finance.DecisionMaking/Classes/DecisionMaker.cs
+++ BJK.Finance.DecisionMaking/Classes/DecisionMaker.cs
@@ -10,10 +10,12 @@
         public IPersonalData PersonalDataConfig { get; } = Personal;
         public IEnumerable<IFinanceInstrument> InstrumentsInformation { get; } = InstrumentData;
         public IEnumerable<IOptionStrategyPossibility> PossibleOptionsStrategies => possibleMoves;
+        public bool IncludeCoveCalls { get; set; } = true;
+        public bool IncludeCashSecuredPuts { get; set; } = true;
         public void BuildStrategies()
         {
             int contactsWillingToBuy = PersonalDataConfig.MinumumUnitsToBuy / 100;
-            
+
             List<IOptionStrategyPossibility> coverCalls = new();
             List<IOptionStrategyPossibility> cashSecuredPuts = new();
 
@@ -28,8 +30,15 @@
                 cashSecuredPuts.Add(cashSecuredPut);
             }
 
-            possibleMoves.AddRange(coverCalls.Where(s => s.ContractsCanAfford >= contactsWillingToBuy && PersonalDataConfig.RatingsTolerance.ToList().Contains(s.FinanceInstrument.AnalystRating)));
-            possibleMoves.AddRange(cashSecuredPuts.Where(s => s.ContractsCanAfford >= contactsWillingToBuy && PersonalDataConfig.RatingsTolerance.ToList().Contains(s.FinanceInstrument.AnalystRating)));
+            if (IncludeCoveCalls)
+            {
+                possibleMoves.AddRange(coverCalls.Where(s => s.ContractsCanAfford >= contactsWillingToBuy && PersonalDataConfig.RatingsTolerance.ToList().Contains(s.FinanceInstrument.AnalystRating)).OrderByDescending(s => s.ContractsCanAfford));
+            }
+
+            if (IncludeCashSecuredPuts)
+            {
+                possibleMoves.AddRange(cashSecuredPuts.Where(s => s.ContractsCanAfford >= contactsWillingToBuy && PersonalDataConfig.RatingsTolerance.ToList().Contains(s.FinanceInstrument.AnalystRating)).OrderByDescending(s => s.ContractsCanAfford));
+            }
         }
     }
 }
